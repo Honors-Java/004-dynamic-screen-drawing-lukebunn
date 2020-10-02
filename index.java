fab83d21fbe1b13b448b013ea5c079238fead9f6
lukebunn1 <superpantsftw1@gmail.com>
2020-10-02 15:09:27 +0000

    I added mousepressed and keypressed

--- index.java
+++ index.java
@@ -1,9 +1,10 @@
-void setup() {
+ void setup() {
 	size(300, 300);
+  background(155);
 }
 
 void draw(){
-    
+   
 
 
 
@@ -11,3 +12,12 @@ void draw(){
 }
 
 //dont forget to add mouse and keypressed interaction functions
+void mousePressed()  { 
+      stroke(0);
+      fill(mouseX, 0);
+      rect(mouseX, mouseY, 40, 30);
+      rectMode(CENTER);
+    }
+void keyPressed() {
+  background(0,50,0,0);
+}
